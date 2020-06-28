28dd2660e198017e822e866216591948ba6fa081
Luis Cervantes <luiscerv1229@gmail.com>
2020-06-28 09:15:05 -0700

    added mysql data

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+USE employee_tracker_db;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Steph", "Curry", 2, 1);
